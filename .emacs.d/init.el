bebb87aff0e0148e3da85236c33cd3b656c4bb11
Josh Bohde <joshbohde@hms-beatdown.(none)>
2009-08-22 01:47:43 -0500

    Took away gui. Added kill line.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,9 @@
+(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+        (let* ((my-lisp-dir "~/.emacs.d/")
+              (default-directory my-lisp-dir))
+           (setq load-path (cons my-lisp-dir load-path))
+           (normal-top-level-add-subdirs-to-load-path)))
+
 (require 'quack)
 
 (require 'paredit)
@@ -7,3 +13,12 @@
 				    "-mode-hook"))))
 	  (add-hook hook (lambda () (paredit-mode +1)))))
       '(emacs-lisp lisp inferior-lisp inferior-scheme))
+
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
