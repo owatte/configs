6b686949c33d2d319f52153ba1aab06484205032
Josh Bohde <josh.bohde@gmail.com>
2009-09-01 12:36:51 -0500

    Started using org-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,6 +17,17 @@
 (require 'ido)
 (ido-mode t)
 
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+(setq org-agenda-files (list "~/org/kmnr.org"
+                             "~/org/school.org" 
+                             "~/org/personal.org"))
+
+
+
 (mapc (lambda (mode)
 	(let ((hook (intern (concat (symbol-name mode)
 				    "-mode-hook"))))
